4a10e154955bc548c86268be116b370414f49673
Neil Toledo <ntoledo@berkeley.edu>
2020-12-06 20:37:47 -0800

    init commit

---
+++ client-build/precache-manifest.90cc67cf71ce8d36d1e3eac94118a341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4023deb48833adfb3cb2c938b22726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bcc24ad9dcaeddea195",
+    "url": "/static/css/main.ca02da1d.chunk.css"
+  },
+  {
+    "revision": "1b7430a6a28693f64ff6",
+    "url": "/static/js/2.76e53466.chunk.js"
+  },
+  {
+    "revision": "eb1e786f062838713c5ee0383afecfb3",
+    "url": "/static/js/2.76e53466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bcc24ad9dcaeddea195",
+    "url": "/static/js/main.cfdf6c3b.chunk.js"
+  },
+  {
+    "revision": "8dac8e0dc503d24d1688",
+    "url": "/static/js/runtime-main.5f3c45d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
